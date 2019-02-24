458cadbfc54690eb5e9f8aff40f9b99b32af2682
San Hortiangtham <ah00104@gmail.com>
2019-02-24 10:43:00 -0500

    small edit

--- datacamp/02_intermediate_python/ch03_logic_control_filtering/filtering_pd_dataframe.py
+++ datacamp/02_intermediate_python/ch03_logic_control_filtering/filtering_pd_dataframe.py
@@ -2,7 +2,6 @@
 ###		>> Filtering Pandas DataFrame
 
 
-
 if False:
 	import pandas as pd
 	
@@ -34,7 +33,6 @@ if False:
 	print brics_new, '\n'
 
 
-
 import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
 print cars
@@ -50,7 +48,6 @@ print (sel)
 
 # Create car_maniac: observations that have a cars_per_cap over 500
 car_maniac = cars[cars['cars_per_cap']>500]
-
 # Print car_maniac
 print (car_maniac)
 
@@ -58,9 +55,6 @@ print (car_maniac)
 cpc = cars['cars_per_cap'] # pandas Series
 between = np.logical_and(cpc > 100, cpc < 500)
 medium = cars[between]
-
-
-
 # Print medium
 print (medium)
 
